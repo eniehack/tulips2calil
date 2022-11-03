58db17e91a1dfe217c67181103092ff98e0db930
Nakaya <eniehack@outlook.jp>
2022-11-03 23:30:35 +0900

    first commit

---
+++ insert_link.js
@@ -0,0 +1,17 @@
+let info_elem = document.getElementById("detailkMenu").querySelector(".c_related_materials_panel_info");
+console.debug("info", info_elem);
+let isbn = document.evaluate("//div[@id=\"lid_major_detail_1\"]//dt[text()=\"ISBN\"]/following-sibling::dd/p/text()", document, null, XPathResult.STRING_TYPE, null).stringValue;
+console.debug("isbn", isbn);
+
+if (isbn !== "") {
+    const node = document.createElement("p");
+    const link_elem = document.createElement("a");
+    link_elem.setAttribute("href", `https://calil.jp/book/${isbn}`);
+    link_elem.setAttribute("target", "_blank");
+    link_elem.setAttribute("rel", "noreferer noopener");
+    link_elem.appendChild(document.createTextNode("calil"));
+    node.appendChild(link_elem);
+    console.log("node", node);
+    info_elem.appendChild(node);
+}
+
