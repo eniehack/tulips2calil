3deedc82339b5f92786bf5274ae8e629bf7bd863
Nakaya <eniehack@outlook.jp>
2022-11-04 01:54:24 +0900

    fix: remove console.debug

--- insert_link.js
+++ insert_link.js
@@ -1,7 +1,5 @@
 let info_elem = document.getElementById("detailkMenu").querySelector(".c_related_materials_panel_info");
-console.debug("info", info_elem);
 let isbn = document.evaluate("//div[@id=\"lid_major_detail_1\"]//dt[text()=\"ISBN\"]/following-sibling::dd/p/text()", document, null, XPathResult.STRING_TYPE, null).stringValue;
-console.debug("isbn", isbn);
 
 if (isbn !== "") {
     const node = document.createElement("p");
